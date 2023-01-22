ee18b1b1d13e175b873d16168e4de5d342b9f93d
Juki <judekitch@gmail.com>
2023-01-22 13:45:06 +1300

    Update tournament.py

--- ao/tournament.py
+++ ao/tournament.py
@@ -35,7 +35,7 @@ TeamGelatoGiants.event(mens_singles).match("1.4").winner("Auger-Aliassime").in_s
 TeamGelatoGiants.event(mens_singles).match("1.5").winner("Rublev").in_sets(3)
 TeamGelatoGiants.event(mens_singles).match("1.6").winner("Djokovic").in_sets(4)
 TeamGelatoGiants.event(mens_singles).match("1.7").winner("Wolf").in_sets(3)
-TeamGelatoGiants.event(mens_singles).match("1.8").winner("Bautista-Agut).in_sets(4)
+TeamGelatoGiants.event(mens_singles).match("1.8").winner("Bautista-Agut").in_sets(4)
 
 TeamGelatoGiants.event(womens_singles).match("1.1").winner(Swiatek).in_sets(2)
 TeamGelatoGiants.event(womens_singles).match("1.2").winner(Gauff).in_sets(2)
@@ -55,7 +55,7 @@ TeamPolarPrecision.event(mens_singles).match("1.4").winner("Auger-Aliassime").in
 TeamPolarPrecision.event(mens_singles).match("1.5").winner("Rublev").in_sets(4)
 TeamPolarPrecision.event(mens_singles).match("1.6").winner("deMinaur").in_sets(5)
 TeamPolarPrecision.event(mens_singles).match("1.7").winner("Shelton").in_sets(5)
-TeamPolarPrecision.event(mens_singles).match("1.8").winner("Bautista-Agut).in_sets(5)
+TeamPolarPrecision.event(mens_singles).match("1.8").winner("Bautista-Agut").in_sets(5)
 
 TeamPolarPrecision.event(womens_singles).match("1.1").winner(Rybakina).in_sets(3)
 TeamPolarPrecision.event(womens_singles).match("1.2").winner(Gauff).in_sets(3)
@@ -75,7 +75,7 @@ TeamHeroHangouts.event(mens_singles).match("1.4").winner("Auger-Aliassime").in_s
 TeamHeroHangouts.event(mens_singles).match("1.5").winner("Rune").in_sets(5)
 TeamHeroHangouts.event(mens_singles).match("1.6").winner("Djokovic").in_sets(5)
 TeamHeroHangouts.event(mens_singles).match("1.7").winner("Wolf").in_sets(4)
-TeamHeroHangouts.event(mens_singles).match("1.8").winner("Bautista-Agut).in_sets(5)
+TeamHeroHangouts.event(mens_singles).match("1.8").winner("Bautista-Agut").in_sets(5)
 
 TeamHeroHangouts.event(womens_singles).match("1.1").winner(Swiatek).in_sets(3)
 TeamHeroHangouts.event(womens_singles).match("1.2").winner(Gauff).in_sets(3)
@@ -95,7 +95,7 @@ TeamBearNecessities.event(mens_singles).match("1.4").winner("Auger-Aliassime").i
 TeamBearNecessities.event(mens_singles).match("1.5").winner("Rublev").in_sets(4)
 TeamBearNecessities.event(mens_singles).match("1.6").winner("deMinaur").in_sets(5)
 TeamBearNecessities.event(mens_singles).match("1.7").winner("Wolf").in_sets(5)
-TeamBearNecessities.event(mens_singles).match("1.8").winner("Paul).in_sets(5)
+TeamBearNecessities.event(mens_singles).match("1.8").winner("Paul").in_sets(5)
 
 TeamBearNecessities.event(womens_singles).match("1.1").winner(Rybakina).in_sets(3)
 TeamBearNecessities.event(womens_singles).match("1.2").winner(Ostapenko).in_sets(3)
