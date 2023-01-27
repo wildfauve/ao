ad9d353a1a188cb1d85915ed347a8663e0c6b072
Col Perks <wild.fauve@gmail.com>
2023-01-28 10:43:55 +1300

    update ttl

--- ao/graph/graph.py
+++ ao/graph/graph.py
@@ -4,17 +4,18 @@ from pathlib import Path
 from . import binding
 
 BASE_PATH = (Path(__file__).parent.parent / "ontology")
+EVENT_BASE_PATH = (Path(__file__).parent.parent / "majors")
 
 TENNIS_ONTO = (BASE_PATH / "ontology.ttl")
 PLAYERS_IND = (BASE_PATH / "players.ttl")
-TOURNAMENT_IND = (BASE_PATH / "tournaments.ttl")
+AO2023 = (EVENT_BASE_PATH / "year_2023" / "australian_open" / "triple_store" / "ao2023.ttl")
 
 
 def graph():
     g = initgraph()
     g.parse(TENNIS_ONTO)
     g.parse(PLAYERS_IND)
-    g.parse(TOURNAMENT_IND)
+    g.parse(AO2023)
     return g
 
 
